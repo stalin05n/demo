9186aa7b76ab428c8d6c671b03e6f80bd8e2d041
stalin2005K <se4715723@gmail.com>
2025-04-30 02:32:28 -0700

    index.js

---
+++ java script/index.js
@@ -0,0 +1,36 @@
+// let a =8
+// let b =21
+// console.log(a+b);
+// let c="hello"
+// let d="world"
+// console.log(c+d)
+// console.log(c.length)
+// console.log(d.length)
+// let color="yellow"
+// let lastName="sairam"
+// console.log(color+ "" +lastName)
+// let e="20"
+// let f="45"
+// console.log(e-f)
+// console.log(e%f)
+// console.log(e*f)
+// console.log(e^f)
+// let x= true;
+// let y= false;
+// console.log(x + y);
+// console.log(x && y);
+// function add( a,b){
+//     return a+b 
+// }
+// console.log(add(5,8));
+// const numbers=[ 1,2,3,4,5];
+// const ans=numbers.map((num)=>num*2);
+// console.log(ans);
+// const even=numbers.filter((num) =>num%2==0);
+// console.log(evan)
+const name ="sairam";
+const age=38;
+const message=`my name is ${name} and my age is ${age}`;
+console.log (message);
+
+
